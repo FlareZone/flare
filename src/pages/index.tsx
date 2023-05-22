9cdaf5c60789f1ff7a5f4fcd7fc0b4f12576443b
jokereven <zjing125.cn@gmail.com>
2023-05-22 22:33:09 +0800

    feat: over base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,106 +1,48 @@
 import AppearanceSwitch from "@/components/part/appearance-switch"
 import LanguageSwitch from "@/components/part/language-switch"
-import { Card, CardTitle, SelectedCard } from "@/components/ui/card"
-import { albumAtom, incAndDecAtom, photosStateAtom } from "@/state/demo"
-import { useAtom } from "jotai"
-import { Fragment, Suspense } from "react"
 import { useTranslation } from "react-i18next"
-
-function Controller() {
-	const [id, setId] = useAtom(incAndDecAtom)
-	return (
-		<div className="flex items-center gap-2">
-			{id > 1 && (
-				<button onClick={() => setId("dec")}>
-					<div className="i-carbon-arrow-left"></div>
-				</button>
-			)}
-			<span>{id}</span>
-			{id < 10 && (
-				<button onClick={() => setId("inc")}>
-					<div className="i-carbon-arrow-right"></div>
-				</button>
-			)}
-		</div>
-	)
-}
-
-function Photos() {
-	const [photos] = useAtom(photosStateAtom)
-
-	if (!photos.data) {
-		return (
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-3xl">
-				{Array.from({ length: 9 }).map((_v, index) => {
-					const uniqueId = `${index}`
-					return (
-						<Fragment key={uniqueId}>
-							<Card id={uniqueId}>
-								<CardTitle className="flex items-center gap-2">
-									{"Loading... "}
-									<div className="i-carbon-circle-dash animate-spin"></div>
-								</CardTitle>
-							</Card>
-						</Fragment>
-					)
-				})}
-			</div>
-		)
-	}
-
-	const photoSet = photos.data
-		.filter((photo, index) => {
-			return (
-				index ===
-				photos.data.findIndex((obj) => {
-					return obj.id === photo.id && obj.albumId === photo.albumId
-				})
-			)
-		})
-		.slice(0, 9)
-
-	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-3xl">
-			{photoSet.map((photo, index) => {
-				const uniqueId = `${index}`
-				return (
-					<Fragment key={uniqueId}>
-						<Card id={uniqueId}>
-							<CardTitle>{photo.title}</CardTitle>
-						</Card>
-						<SelectedCard id={uniqueId} className="w-80 h-60">
-							<img
-								src={photo.thumbnailUrl}
-								alt={photo.title}
-								className="w-full h-full object-cover"
-							></img>
-						</SelectedCard>
-					</Fragment>
-				)
-			})}
-		</div>
-	)
-}
-
-function AlbumInfo() {
-	const [album] = useAtom(albumAtom)
-	return <h1>{album?.title}</h1>
-}
 
 export default function App() {
 	const { t } = useTranslation()
 
 	return (
-		<div className="p-4 flex flex-col gap-4 items-center justify-center font-mono">
-			<h1>{t("hello-world")}</h1>
-			<Suspense fallback="loading...">
-				<AlbumInfo />
-			</Suspense>
-			<Controller />
-			<Photos />
-			<div className="flex gap-4 items-center">
-				<AppearanceSwitch />
-				<LanguageSwitch />
+		<div className="flex flex-col gap-4 items-start font-mono text-size-lg p-2">
+			<div
+				className="flex flex-row gap-4 items-start w-full justify-between items-center"
+				style={{ padding: "0 1rem 0 1rem" }}
+			>
+				<img
+					className="hover: cursor-pointer"
+					style={{ width: 64, height: 64 }}
+					src={"/public/logo.png"}
+					alt="Flare Dapp"
+				/>
+				<div className="hover: cursor-pointer">Home</div>
+				<div>Search</div>
+				<div className="flex flex-row gap-4">
+					<h1>{t("language")}</h1>
+					<LanguageSwitch />
+					<AppearanceSwitch />
+				</div>
+				<div>
+					<a href="/dashboard" target={"_blank"}>
+						Dashboard
+					</a>
+				</div>
+				<div className="hover: cursor-pointer">Prefile</div>
+			</div>
+			<div className="flex flex-col p-xy gap-4xl">
+				<div>Create Post</div>
+				<ul className="flex gap-8xl justify-start">
+					<li>Best</li>
+					<li>Hot</li>
+					<li>New</li>
+					<li>Top</li>
+					<li>Rising</li>
+				</ul>
+				<div style={{ position: "fixed", bottom: 0, width: "100%" }}>
+					LIVE DATA ACTIVE
+				</div>
 			</div>
 		</div>
 	)
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,15 @@
 import App from "@/pages"
+import Dashboard from "@/pages/dashboard"
 import { createBrowserRouter } from "react-router-dom"
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+	},
+	{
+		path: "/dashboard",
+		element: <Dashboard />,
 	},
 ])
 
